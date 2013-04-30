9cea8ef6e961f5334dda4ffc881ecde972414903
Aaron Pollack <pollackaaron@gmail.com>
2013-04-29 22:21:46 -0700

    Got rid of un-needed part of app

--- Import test.py
+++
@@ -1,7 +0,0 @@
-from login_window import * # brings in loginwindow
-from Welcome_Window import * # brings in Welcome_Window
-from Upload_window import * #brings in upload window
-
-
-Test = loginwindow(Tk())
-Test.run()
